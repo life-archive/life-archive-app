0484dfda841b7a0abf96e1a9506ff577616d2494
Madhan Kanagavel <madhankk@gmail.com>
2026-07-15 13:16:34 -0500

    fix error

--- src/app/ArchiveImage.tsx
+++ src/app/ArchiveImage.tsx
@@ -2,7 +2,11 @@ import Image, { type ImageProps } from "next/image";
 
 import { rendererDefaults } from "@/defaults";
 
-const isMultiHostMode = rendererDefaults.archiveRouting.mode === "multi-host";
+type ArchiveRoutingMode = "single" | "multi-host";
+
+const archiveRoutingMode = rendererDefaults.archiveRouting
+  .mode as ArchiveRoutingMode;
+const isMultiHostMode = archiveRoutingMode === "multi-host";
 
 export function ArchiveImage(props: ImageProps) {
   const { alt, unoptimized, ...imageProps } = props;
